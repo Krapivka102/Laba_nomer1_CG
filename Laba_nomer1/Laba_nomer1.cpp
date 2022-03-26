4613d621a4ee6e88eeb2375e27da74bd460f22c7
Krapivka <niko_wellick@mail.ru>
2022-03-26 17:23:42 +0500

    Добавил код в 1 этап

--- Laba_nomer1/Laba_nomer1.cpp
+++ Laba_nomer1/Laba_nomer1.cpp
@@ -1,10 +1,32 @@
 ﻿#include <iostream>
 #include <GL/freeglut.h>
 
+static void RenderSceneCB()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glutSwapBuffers();
+}
+
+static void InitializeGlutCallbacks()
+{
+    glutDisplayFunc(RenderSceneCB);
+}
 
 int main(int argc, char ** argv)
 {
     glutInit(&argc, argv);
 
+    glutInitWindowSize(1024, 768);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Tutorial 01");
+
+    glutDisplayFunc(RenderSceneCB);
+
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+    glutMainLoop();
+
+    glClear(GL_COLOR_BUFFER_BIT);
+    glutSwapBuffers();
 }
 
